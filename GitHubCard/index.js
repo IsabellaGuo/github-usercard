ae2f24256ca55e6a84f81dd907601effdc52e37d
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-05 15:39:05 -0800

    axios done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,12 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/IsabellaGuo')
+.then (response => {
+  console.log('data: ', data)
+  const myInfo = data.data;
+  console.log('UserInfo: ', myInfo)
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
